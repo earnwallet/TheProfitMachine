f9c93e609d0efc4cacc7b911ab024d0f99849fef
Cyjan <MrCyjaneK@Gmail.com>
2019-08-21 14:14:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ cat info/00-install.txt
 read -p "Are you sure you want to install? <y/N> " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
+  echo "Okey";
   # http://stackoverflow.com/questions/1537673/how-do-i-forward-parameters-to-other-command-in-bash-script
 else
   exit 0
