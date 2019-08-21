3ffa040b005ad78bc250743dad849733ca330ec7
Cyjan <MrCyjaneK@Gmail.com>
2019-08-21 14:15:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "####### #    # #    # #    #  ## ##  #    # ###### ###### ######   #";
 echo "                                                                  ";
 cat info/00-install.txt
 read -p "Are you sure you want to install? <y/N> " prompt
-if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+if [ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
 then
   echo "Okey";
   # http://stackoverflow.com/questions/1537673/how-do-i-forward-parameters-to-other-command-in-bash-script
