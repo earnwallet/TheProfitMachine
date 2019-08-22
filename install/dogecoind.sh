85544d1c149a5f0e157fb7072a6f08048757be15
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 01:16:24 +0300

    Update dogecoind.sh

--- install/dogecoind.sh
+++ install/dogecoind.sh
@@ -1,32 +1,22 @@
 #!/bin/sh
 echo "Step 0. Install software.";
 apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y;
-read -p "Is it ok? <y/N> " prompt
-if [ $prompt == "y" ]
-then
-  echo "Ok.";
-  # http://stackoverflow.com/questions/1537673/how-do-i-forward-parameters-to-other-command-in-bash-script
-else
-  echo "Fuck";
-  exit 0
-fi
-mkdir software
-cd software
+cd ~
 git clone https://github.com/dogecoin/dogecoin
-cd dogecoin
-BITCOIN_ROOT=$(pwd)
+cd ~/dogecoin
+BITCOIN_ROOT="~/dogecoin"
 # Pick some path to install BDB to, here we create a directory within the dogecoin directory
 BDB_PREFIX="${BITCOIN_ROOT}/db5"
 mkdir -p $BDB_PREFIX
 # Fetch the source and verify that it is not tampered with
-wget 'http://download.oracle.com/berkeley-db/db-5.1.29.NC.tar.gz'
-echo '08238e59736d1aacdd47cfb8e68684c695516c37f4fbe1b8267dde58dc3a576c db-5.1.29.NC.tar.gz' | sha256sum -c
+
+cp ~/TPM/install/bdb.tar.gz $BDB_PREFIX/bdb.tar.gz
 # -> db-5.1.29.NC.tar.gz: OK
-tar -xzvf db-5.1.29.NC.tar.gz
+tar -xzvf bdb.tar.gz
 # Build the library and install to our prefix
 cd db-5.1.29.NC/build_unix/
 #  Note: Do a static build so that it can be embedded into the executable, instead of having to find a .so at runtime
-../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX --build=x86_64 #Replace with your build if needed. 
 make install
 # Configure Dogecoin Core to use our own-built instance of BDB
 cd $BITCOIN_ROOT
