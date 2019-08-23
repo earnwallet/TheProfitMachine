cbfe7a2cccf4d2b46cec28075b16ee6836057d8d
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 20:45:40 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,4 +1,7 @@
 apt update; sudo apt install git -y;
 git clone https://github.com/earnwallet/TheProfitMachine TPM
-echo "to start program run:";
-echo "cd TPM && sh install.sh";
+cd TPM && sh install.sh
+echo "Installation complete.";
+echo "before using tool you will more likely need to download some blockchain stuff, about 20gb... Do this by running blocksync after launching tpm";
+echo "feel free to close terminal now.";
+exit
