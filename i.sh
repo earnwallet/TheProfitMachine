8d3751c839bd56a4e4d283442af6720a652244fb
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 10:32:40 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,4 +1,4 @@
 apt update; apt install git -y;
-git clone --depth=1 https://github.com/earnwallet/TheProfitMachine TPM
+git clone https://github.com/earnwallet/TheProfitMachine TPM
 echo "to start program run:";
 echo "cd TPM && sh install.sh";
