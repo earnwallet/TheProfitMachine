fea2828d99f367ff557584fbfc5bb57f716a8c46
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 10:54:56 +0300

    Update dogecoind.sh

--- install/dogecoind.sh
+++ install/dogecoind.sh
@@ -2,20 +2,21 @@
 echo "Step 0. Install software.";
 apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y;
 cd ~
+pwd=$(pwd)
 git clone https://github.com/dogecoin/dogecoin
 cd ~/dogecoin
-BITCOIN_ROOT="~/dogecoin"
+BITCOIN_ROOT="$pwd/dogecoin"
 # Pick some path to install BDB to, here we create a directory within the dogecoin directory
 BDB_PREFIX="${BITCOIN_ROOT}/db5"
 mkdir -p $BDB_PREFIX
 # Fetch the source and verify that it is not tampered with
 
-cp ~/TPM/install/bdb.tar.gz $BDB_PREFIX/bdb.tar.gz
+cp $pwd/TPM/install/bdb.tar.gz $BDB_PREFIX/bdb.tar.gz
 cd $BDB_PREFIX
 # -> db-5.1.29.NC.tar.gz: OK
 tar -xzvf bdb.tar.gz
 # Build the library and install to our prefix
-cd db-5.1.29.NC/build_unix/
+cd $pwd/db-5.1.29.NC/build_unix/
 #  Note: Do a static build so that it can be embedded into the executable, instead of having to find a .so at runtime
 ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX --build=x86_64 #Replace with your build if needed. 
 make install
