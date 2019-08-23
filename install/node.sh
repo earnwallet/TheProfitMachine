553423e2334a35e650f95d23aaf608dc56dcce4f
mrcyjanek <mrcyjanek@gmail.com>
2019-08-23 15:53:39 +0000

    k

--- install/node.sh
+++ install/node.sh
@@ -1,21 +1,11 @@
-echo "checking if dogecoin core is installed";
+echo "checking if node is installed";
 start=`date +%s`;
 timeout 10 speed-test;
 end=`date +%s`
 tim=`expr $end - $start`
 if [ $tim -gt 9 ]; then
-    echo "Dogecoin core is installed";
+    echo "node is installed";
 else
-    echo "Not enough doge, need to make more";
-echo "checking if dogecoin core is installed";
-start=`date +%s`;
-timeout 10 dogecoind -printtoconsole;
-end=`date +%s`
-tim=`expr $end - $start`
-if [ $tim -gt 9 ]; then
-    echo "Dogecoin core is installed";
-else
-    echo "Not enough doge, need to make more";
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo npm i -g speed-test
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo npm i -g speed-test
 fi
