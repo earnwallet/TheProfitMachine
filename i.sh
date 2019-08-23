29a08d16dabf39bbbe8f4beeb7fa3fac67c6022b
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 22:33:34 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+echo " = = = = License = = = = ";
+curl https://raw.githubusercontent.com/earnwallet/TheProfitMachine/master/license.txt;
+echo " = = = =  E N D  = = = = ";
+echo "Do you accept this license? [Y/n]"
+read a
+echo $a
+if [ "$a" == "n" ]; then
+    echo "xD"
+    exit
+fi
 apt update; sudo apt install git -y;
 git clone https://github.com/earnwallet/TheProfitMachine TPM
-cd TPM && sh install.sh
+cd TPM && bash install.sh
 echo "Installation complete.";
 echo "before using tool you will more likely need to download some blockchain stuff, about 20gb... Do this by running blocksync after launching tpm";
 echo "feel free to close terminal now.";
