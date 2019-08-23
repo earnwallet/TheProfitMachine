1ed4ad427e611eef2d11ea7c2eec80c0399e6740
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 19:03:09 +0300

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -7,5 +7,12 @@ if [ $tim -gt 9 ]; then
     echo "node is installed";
 else
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt-get install -y nodejs` to install Node.js 12.x and npm
+## You may also need development tools to build native addons:
+     sudo apt-get install gcc g++ make -y
+## To install the Yarn package manager, run:
+     curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+     sudo apt-get update && sudo apt-get install yarn -y
     sudo npm i -g speed-test
 fi
