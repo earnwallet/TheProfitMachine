73f870e1beacd8b748dc30ecaf2078b864ec96b9
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 23:40:19 +0300

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -6,9 +6,9 @@ tim=`expr $end - $start`
 if [ $tim -gt 2 ]; then
     echo "node is installed";
 else
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt-get install -y nodejs npm
-## You may also need development tools to build native addons:
-    sudo apt-get install gcc g++ make -y
+    # You may also need development tools to build native addons:
+    #sudo apt-get install gcc g++ make -y
     sudo npm i -g speed-test
 fi
