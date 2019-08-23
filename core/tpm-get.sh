ac7e0b7a1df27fdd196040d7a316e851e616d2d2
Cyjan <MrCyjaneK@Gmail.com>
2019-08-23 23:15:51 +0300

    Update tpm-get.sh

--- core/tpm-get.sh
+++ core/tpm-get.sh
@@ -4,7 +4,7 @@ echo "Welcome to TheProfitMachine Package Manager tool."
 echo "=============";
 echo "What would you like to install?";
 echo " (1) Module";
-echo " (2) Add-on";
+echo " (2) Application";
 read cho
 if [ "$cho" == "1" ]; then
     echo "Interesting... Tell me more. Who's the owner?"
@@ -39,5 +39,56 @@ if [ "$cho" == "1" ]; then
         echo "This is not any sort of advice, but next time click Y on your keyboard."
         exit;
     fi
-    
+    cd /home/`whoami`/TPM
+    mkdir mods
+    cd mods
+    echo "Cloning"
+    echo "screems *baa mee*" # sheep was first cloned animal.
+    git clone https://github.com/$username/TPMmod_$package $package
+    echo "Complete! This mod is now available!"
+    sleep 3
+    exit;
+fi
+if [ "$cho" == "2" ]; then
+    echo "Interesting... Tell me more. Who's the owner of this application?"
+    echo "Username:";
+    read username;
+    echo "Hmm, okay. So $username right? I remember that $username... I'll tell you more about that guy later,";
+    echo "Package name:";
+    read package
+    echo "Okay..."
+    echo "So I should install $package created by $username? [Y/n]";
+    read a
+    if [ "$a" == "y" -o "$a" == "Y" ]; then
+        echo "Okay!";
+    else
+        echo "Umm... Okay, let's try again"
+        sleep 2;
+        tpm-get; 
+        exit;
+    fi
+    echo "Before we proceed some paperwork is needed";
+    echo "Read this carefully and tell me do you accept it or no, okay?"
+    # resp=$(curl https://raw.githubusercontent.com/$username/TPMapp_$package/master/license.txt)
+    # TODO: Check if repo exist.
+    echo " - - - - LICENSE - - - - ";
+    curl https://raw.githubusercontent.com/$username/TPMmod_$package/master/license.txt;
+    echo "--------- E N D ---------";
+    echo "Is this fair? [Y/n]";
+    read a
+    if [ "$a" == "y" -o "$a" == "Y" ]; then
+        echo "Okay!";
+    else
+        echo "This is not any sort of advice, but next time click Y on your keyboard."
+        exit;
+    fi
+    cd /home/`whoami`/TPM
+    mkdir mods
+    cd mods
+    echo "Cloning"
+    echo "screems *baa mee*" # sheep was first cloned animal.
+    git clone https://github.com/$username/TPMapp_$package $package
+    echo "Complete! This app is now available!"
+    sleep 3
+    exit;
 fi
