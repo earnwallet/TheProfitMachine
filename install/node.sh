226cfb12ec4e80696bc55c36615cc1e714d92519
Cyjan <MrCyjaneK@Gmail.com>
2019-08-24 21:27:46 +0300

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -1,6 +1,6 @@
 echo "checking if node is installed";
 start=`date +%s`;
-timeout 3 speed-test;
+timeout 3 node -e "setTimeout(() => {console.log("hmm")},99999)!";
 end=`date +%s`
 tim=`expr $end - $start`
 if [ $tim -gt 2 ]; then
@@ -9,6 +9,5 @@ else
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
     sudo apt-get install -y nodejs
     # You may also need development tools to build native addons:
-    #sudo apt-get install gcc g++ make -y
-    sudo npm i -g speed-test
+    sudo apt-get install gcc g++ make -y
 fi
