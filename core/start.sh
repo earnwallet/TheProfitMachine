913e81142e914af6e6344b800582427f89f1ddde
Cyjan <MrCyjaneK@Gmail.com>
2019-08-25 18:09:27 +0300

    Update start.sh

--- core/start.sh
+++ core/start.sh
@@ -1,6 +1,6 @@
 git pull
 echo "Starting Dogecoin Wallet.";
 dogecoind -daemon
-source /home/`whoami`/TPM/core/alias.sh
+source ~/TPM/core/alias.sh
 screen -dm php -S 127.0.0.1:69042 /home/`whoami`/TPM/core/api
 echo "Done!";
