50c6bf0deeb12ef18b34a7c8a2c0a94ddaa38503
Cyjan <MrCyjaneK@Gmail.com>
2019-08-25 17:38:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,14 +23,15 @@ else
   exit 0
 fi
 start=`date +%s`;
-timeout 2 sleep 5;
+timeout 2 screen sleep 2;
 end=`date +%s`
 tim=`expr $end - $start`
 if [ $tim -gt 1 ]; then
-    echo "timeout is installed";
+    echo "timeout and screen is installed";
 else
     echo "Installing timeout";
     sudo apt-get install timeout -y
+    sudo apt-get install screen -y
 fi
 echo "=============";
 echo "INSTALLING PHP";
