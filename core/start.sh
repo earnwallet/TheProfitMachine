56acd40544229646c88e567b255c0a9d6acbcf29
Cyjan <MrCyjaneK@Gmail.com>
2019-08-25 17:15:31 +0300

    Update start.sh

--- core/start.sh
+++ core/start.sh
@@ -4,6 +4,6 @@ dogecoind -daemon
 echo "initializing tpm-get";
 alias tpm-get="bash /home/`whoami`/TPM/core/tpm-get.sh";
 echo "initializing tpm"
-alias tpm="bash /home/`whoami`/TPM/core/tpm.sh
+alias tpm="bash /home/`whoami`/TPM/core/tpm.sh"
 screen -dm php -S 127.0.0.1:69042 /home/`whoami`/TPM/core/api
 echo "Done!";
