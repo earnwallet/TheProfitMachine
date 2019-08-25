4e15851198f1250a2d085c571ef0504dba4d59dd
Cyjan <MrCyjaneK@Gmail.com>
2019-08-25 17:16:26 +0300

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -1,9 +1,9 @@
 echo "checking if node is installed";
 start=`date +%s`;
-timeout 3 node -e "setTimeout(() => {console.log("hmm")},99999);";
+timeout 2 node -e "setTimeout(() => {console.log("hmm")},99999);";
 end=`date +%s`
 tim=`expr $end - $start`
-if [ $tim -gt 2 ]; then
+if [ $tim -gt 1 ]; then
     echo "node is installed";
 else
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
