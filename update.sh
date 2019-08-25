f5581a327fd606e01838ef791db09de60d40b74a
Cyjan <MrCyjaneK@Gmail.com>
2019-08-25 22:00:23 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+echo "Updating core 1/2";
+cd /home/`whoami`/TPM/
 git pull;
+echo "Updating mods";
+cd /home/`whoami`/TPM/mods;
+for f in *;
+do
+    cd /home/`whoami`/TPM/mods/"$f" && git pull; 
+done
+echo "Updating apps"
+cd /home/`whoami`/TPM/app
+for f in *;
+do
+    cd /home/`whoami`/TPM/app/"$f" && git pull; 
+done
+echo "Updating core 2/2";
 cd /home/`whoami`/TPM
 sh install.sh y
