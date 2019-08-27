75d2b32aa4035d1004e70a55e703fb75d0cf538c
Cyjan <MrCyjaneK@Gmail.com>
2019-08-28 00:30:47 +0300

    Moved

--- core/start.sh
+++ core/start.sh
@@ -1,7 +1,9 @@
 git pull
 echo "Starting Dogecoin Wallet.";
 dogecoind -daemon
-echo "type: source ~/TPM/core/alias.sh";
-#source alias.sh
-screen -dm php -S 127.0.0.1:69042 /home/`whoami`/TPM/core/api
+echo "initializing tpm-get";
+alias tpm-get="bash /home/`whoami`/TPM/core/tpm-get.sh";
+echo "initializing tpm"
+alias tpm="bash /home/`whoami`/TPM/core/tpm.sh"
+# screen -dm php -S 127.0.0.1:69042 /home/`whoami`/TPM/core/api
 echo "Done!";
