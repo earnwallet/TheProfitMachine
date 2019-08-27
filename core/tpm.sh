945a72ca601f951632656ab348a6e7cae29f5f2e
Cyjan <MrCyjaneK@Gmail.com>
2019-08-28 00:29:26 +0300

    Update tpm.sh

--- core/tpm.sh
+++ core/tpm.sh
@@ -1,1 +1,42 @@
 # TODO: put idea from my brain here.
+clear
+echo "==== T P M ====";
+echo "What would you like to start?";
+echo " (1) Application";
+echo " (2) Module";
+read cho;
+if [ "$cho" == "1" ]; then
+    clear;
+    cd /home/`whoami`/TPM
+    mkdir apps;
+    cd apps;
+    for f in *;
+    do
+         echo " (=) $f";
+    done
+    echo "Which application should I start?"
+    read app;
+    cd "$app";
+    echo "==== START ====";
+    sh start.sh;
+    echo "==== APP CLOSED ====";
+    echo " [Exit Code] - $?";
+    exit;
+fi
+if [ "$cho" == "2" ]; then
+    clear;
+    cd /home/`whoami`/TPM
+    mkdir mods;
+    cd mods;
+    for f in *;
+    do
+         echo " (=) $f";
+    done
+    echo "Which application should I start?"
+    read app;
+    cd "$app";
+    echo "==== STARTING... ====";
+    sh start.sh;
+    echo "==== APP STARTED ====";
+    exit;
+fi
