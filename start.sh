86719e1baf4d5d8c14092b4460cafe139700513d
Cyjan <MrCyjaneK@Gmail.com>
2019-08-29 12:45:47 +0200

    Added mod info on start

--- start.sh
+++ start.sh
@@ -1,15 +1,46 @@
 cd /home/`whoami`/TPM
 # bash update.sh
-git pull
+# TODO: Ask user if he want to do update or no.
+# git pull
 echo "Starting Dogecoin Wallet.";
 dogecoind -daemon
+# screen -dm php -S 127.0.0.1:69042 /home/`whoami`/TPM/core/api
+echo "Initializing aliases 1/2*;
 echo "initializing tpm-get";
 alias tpm-get="bash /home/`whoami`/TPM/core/tpm-get.sh";
 echo "initializing tpm"
 alias tpm="bash /home/`whoami`/TPM/core/tpm.sh"
-# screen -dm php -S 127.0.0.1:69042 /home/`whoami`/TPM/core/api
-echo "Done!";
+echo "Initializing external aliases";
 
+cd /home/`whoami`/TPM/apps/
+
+for f in *;
+do
+    echo " (=) app: $f";
+    alias $f="bash /home/`whoami`/TPM/apps/$f/start.sh";
+    echo "   ==== I N F O ==== ";
+    cat "/home/`whoami`/TPM/apps/$f/info.txt";
+    echo "==== END INFO ====";
+done;
+
+echo "==== D O N E ====";
+echo "Starting background modules";
+for f in *;
+do
+    echo " (=) mod: $f";
+    echo "   ==== I N F O ==== ";
+    cat "/home/`whoami`/TPM/mods/$f/info.txt";
+    echo "==== END INFO ====";
+    bash /home/`whoami`/TPM/mods/$f/start.sh;
+    if [ $? == 0 ];
+    then
+        echo "Module $f started successfully";
+    else
+        echo "Non-zero error code occurred, exiting.";
+        sleep 2;
+        exit;
+    fi
+done;
 echo "=============";
 echo "Started, you can use TPM tools now.";
 echo "=============";
