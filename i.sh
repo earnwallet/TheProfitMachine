6772885225620ce76d7a43dc4c854de3c785b44d
Cyjan <MrCyjaneK@Gmail.com>
2019-08-29 12:48:54 +0200

    Exit code 😎

--- i.sh
+++ i.sh
@@ -6,8 +6,8 @@ echo "Do you accept this license? [Y/n]"
 read a
 echo $a
 if [ "$a" == "n" ]; then
-    echo "xD"
-    exit
+    echo "Oh... I'm sorry but you are not allowed to use it."
+    exit 1;
 fi
 apt update; sudo apt install git -y;
 git clone https://github.com/earnwallet/TheProfitMachine TPM
@@ -15,4 +15,4 @@ cd TPM && bash install.sh
 echo "Installation complete.";
 echo "before using tool you will more likely need to download some blockchain stuff, about 20gb... Do this by running blocksync after launching tpm";
 echo "feel free to close terminal now.";
-exit
+exit 0
