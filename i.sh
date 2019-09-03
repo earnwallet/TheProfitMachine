c27a7c0fed4fe6dac3a5f60e6215c0bb69a4c51e
Cyjan <MrCyjaneK@Gmail.com>
2019-09-03 18:16:37 +0200

    Update i.sh

--- i.sh
+++ i.sh
@@ -9,10 +9,16 @@ if [ "$a" == "n" ]; then
     echo "Oh... I'm sorry but you are not allowed to use it."
     exit 1;
 fi
-apt update; sudo apt install git -y;
+sudo apt update; sudo apt install git -y;
 git clone https://github.com/earnwallet/TheProfitMachine TPM
 cd TPM && bash install.sh
+if [ "$?" == 0 ];
+then
 echo "Installation complete.";
 echo "before using tool you will more likely need to download some blockchain stuff, about 20gb... Do this by running blocksync after launching tpm";
 echo "feel free to close terminal now.";
 exit 0
+else
+echo "INSTALLATION FAILED";
+exit 1;
+fi
