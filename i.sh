f01fbbfdb2108e3bec04a43443406128b2259f6e
Cyjan <MrCyjaneK@Gmail.com>
2019-10-29 07:44:04 +0100

    Update i.sh
    +Code formating
    +--recurse-submodules --remote-submodules

--- i.sh
+++ i.sh
@@ -10,15 +10,15 @@ if [ "$a" == "n" ]; then
     exit 1;
 fi
 sudo apt update; sudo apt install git -y;
-git clone https://github.com/earnwallet/TheProfitMachine TPM
+git clone https://github.com/earnwallet/TheProfitMachine TPM --recurse-submodules --remote-submodules
 cd TPM && bash install.sh
 if [ "$?" == 0 ];
 then
-echo "Installation complete.";
-echo "before using tool you will more likely need to download some blockchain stuff, about 20gb... Do this by running blocksync after launching tpm";
-echo "feel free to close terminal now.";
-exit 0
+    echo "Installation complete.";
+    echo "before using tool you will more likely need to download some blockchain stuff, about 20gb... Do this by running blocksync after launching tpm";
+    echo "feel free to close terminal now.";
+    exit 0
 else
-echo "INSTALLATION FAILED";
-exit 1;
+    echo "INSTALLATION FAILED";
+    exit 1;
 fi
